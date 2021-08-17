178851710347f11147d65e04eab691b6b0613b90
clever matchless bravo villafuerte <cleveresclip@hotmail.com>
2021-08-17 15:32:09 -0400

    programa 1

---
+++ programa1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Contenido:"<<endl;
+    cout<<"1.Introducción"<<endl;
+    cout<<"2.Diseño de algoritmos"<<endl;
+    cout<<"3.Estructuras de Control Selectivas"<<endl;
+    cout<<"4.Estructuras de Control Repetitivas"<<endl;
+    cout<<"5.Arrays y Cadenas:"<<endl;
+    cout<<"6.Funciones"<<endl;
+    cout<<"7.Estructuras"<<endl;
+
+    cout<<"\n\n\n\nBibliografía\n";
+    cout<<"*Aprendiendo a programar en C++,Clever Bravo\n";
+    cout<<"*http://conclase.net/c,comunidad\n";
+    cout<<"*Competitive Programming,Steven Halim\n";
+    cout<<"*Introduccion a la programacion, Luis Joyanes\n";
+    cout<<"*Programacion en C++, Luis Joyanes\n";
+    cout<<"*http://biblioteca.uatf.edu.bo/jspui/\n";
+    cout<<"*username: estudiante@uatf.edu.bo\n";
+    cout<<"*password: tomasfrias\n";
+    cout<<"*The art of computer programming, Donald Knuth\n";
+
+    cout<<"*GitHub, repositorio de codigo.\n";
+    cout<<"GitLab, repositorio de codigo.\n";
+    return 0;
+}
